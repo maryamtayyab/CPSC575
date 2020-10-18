61d013b1189680b473583711050c0f2e82698eb6
MaryamTayyab <maryamtayyab95@gmail.com>
2020-10-17 21:55:10 -0600

    Initial Commit

---
+++ WhatsWhere/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsWhere
+//
+//  Created by Maryam TayyabII on 2020-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsWhere/WhatsWhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsWhereApp.swift
+//  WhatsWhere
+//
+//  Created by Maryam TayyabII on 2020-10-17.
+//
+
+import SwiftUI
+
+@main
+struct WhatsWhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
