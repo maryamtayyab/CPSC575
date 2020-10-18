d4a10844de2032e49166b3a7690bd7cdd8a022dd
MaryamTayyab <maryamtayyab95@gmail.com>
2020-10-18 03:23:58 -0600

    Camera Working

--- WhatsWhere/ContentView.swift
+++ WhatsWhere/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var imageData : Data = .init(capacity:0)
+    @State var show = false
+    @State var imagepicker = false
+    @State var source : UIImagePickerController.SourceType = .photoLibrary
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            ZStack {
+                
+                NavigationLink(destination: Imagepicker(show: $imagepicker, image: $imageData, source: source),isActive: $imagepicker) {
+                    
+                    Text(" ")
+                }
+                
+                VStack {
+                    if imageData.count != 0 {
+                        
+                        Image(uiImage: UIImage(data: self.imageData)!).resizable().frame(width:250, height:250).cornerRadius(15)
+                    }
+                    
+                    Button(action: {
+                        
+                        self.show.toggle()
+                        
+                    }) {
+                        Text("Pic")
+                    }
+                    
+                    
+                    
+                }.navigationBarTitle("", displayMode: .inline)
+                .navigationBarHidden(true)
+                .actionSheet(isPresented: $show) {
+                    
+                    ActionSheet(title: Text(""), message: Text (""), buttons: [.default(Text("Upload"), action: {
+                        
+                        self.source = .photoLibrary
+                        self.imagepicker.toggle()
+                    }), .default(Text("Take a Picture"), action: {
+                        
+                        self.source = .camera
+                        self.imagepicker.toggle()
+                    })])
+                }
+            }
+            
+         
+        }
+        
+       
     }
 }
 
@@ -19,3 +67,52 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Imagepicker: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Imagepicker.Coordinator {
+       
+        return Imagepicker.Coordinator(parent1: self)
+    }
+    
+    @Binding var show : Bool
+    @Binding var image : Data
+    var source : UIImagePickerController.SourceType
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<Imagepicker>) -> UIImagePickerController {
+        
+        let controller = UIImagePickerController()
+        controller.sourceType = source
+        controller.delegate = context.coordinator
+        return controller
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<Imagepicker>) {
+        
+    }
+    
+    
+    class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        
+        var parent : Imagepicker
+        init(parent1 : Imagepicker) {
+            
+            parent = parent1
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            
+            self.parent.show.toggle()
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            let image = info[.originalImage] as! UIImage
+            let data = image.pngData()
+            self.parent.image = data!
+            self.parent.show.toggle()
+        }
+    }
+}
+
--- WhatsWhere/WhatsWhereApp.swift
+++ WhatsWhere/WhatsWhereApp.swift
@@ -15,3 +15,9 @@ struct WhatsWhereApp: App {
         }
     }
 }
+
+struct WhatsWhereApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
